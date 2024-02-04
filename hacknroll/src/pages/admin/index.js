ee14ea51448c1390e8b3eaf9fd2509dbe11091c3
Kenneth-Mah <kennethmah@rocketmail.com>
2024-02-04 23:34:12 +0800

    feat: create Admin form template linked to DB

--- hacknroll/src/pages/admin/index.js
+++ hacknroll/src/pages/admin/index.js
@@ -1,6 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import EnhancedTable from "@/components/common/table";
+import { createAdmin } from "@/services/adminService";
 
 export default function Index() {
-  return <></>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const added = await createAdmin(username, password, name);
+    if (added) {
+      setUsername("");
+      setPassword("");
+      setName("");
+
+      alert("Data added to Firestore DB!");
+    }
+  }
+
+  return (
+    <div className="ml-96">
+      <h1 className="text-black">Hello World!</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label className="text-black">Username:</label>
+          <input
+            className="text-black"
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label className="text-black">Password:</label>
+          <input
+            className="text-black"
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label className="text-black">Name:</label>
+          <input
+            className="text-black"
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+
+        <button 
+          className="text-black"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
